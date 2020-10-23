e1f3cb9b5643fc8980c193603b2dc3cf09122759
Laian de Souza Braum <61033391+laianbraum@users.noreply.github.com>
2020-10-22 22:02:27 -0300

    Update L2_21.c

--- L2_21.c
+++ L2_21.c
@@ -1,40 +1,43 @@
-//exercicio nao finalizado
 #include <stdio.h>
-#include <ctype.h>
 
 int main(){
-    int jumanji, contador_aspas = 0, breaker = 0, quantidade_numeros, i = 0;
+    int contador_aspas = 0, breaker = 0, quantidade_numeros, i = 1, j;
     char Caractere, Caractere_anterior;
     scanf("%d", &quantidade_numeros);
-    char numeros[quantidade_numeros];
-    for(i; i < quantidade_numeros; i++){
-        scanf("%c", &numeros[i]);
+    int numeros[quantidade_numeros+1];
+    numeros[0] = 0;
+    for(i; i < quantidade_numeros+1; i++){
+        scanf("%d", &numeros[i]);
     }
     while(breaker == 0){
         scanf("%c", &Caractere);
-        if (Caractere == 34){
-            contador_aspas++;
-            if (contador_aspas == 2){
-                break;
-            }
-            continue;
-        }
-        if (Caractere == '%'){
-            Caractere_anterior = '%';
-            continue;
-            
-        }else if(Caractere_anterior == '%'){
-            int i = 0;
-            for(i; i < quantidade_numeros; i++){
-                if (Caractere == i+1){
-                    Caractere = numeros[i+1];
+        if (Caractere_anterior == '%'){
+            if (Caractere >= 48 && Caractere <= 57){
+                j = Caractere-48;
+                if (j > quantidade_numeros || j < 1){
+                    printf("ERRO");
+                    Caractere_anterior = 'a';
+                    continue;
+                }
+                printf("%d", numeros[j]);
+		    }
+            Caractere_anterior = 'a';
+        }else{
+            if (Caractere == 34){
+                contador_aspas++;
+                if (contador_aspas == 2)
+                {
+                    break;
+                }
+                continue;
+            }else{
+                if (Caractere == '%'){
+                    Caractere_anterior = '%';
+                    continue;
+                }else{
+                    printf("%c", Caractere);
                 }
             }
-            printf("%c", Caractere);
-            Caractere_anterior = 'a';
-        }
-        else{
-            printf("%c", Caractere);
         }
     }
     return 0;
